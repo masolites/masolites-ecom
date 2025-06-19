6b9d2f1e8f9ac12fb7476bae361297fadd99abe4
masolites <mfmmpcs@gmail.com>
2025-06-19 20:10:37 +0100

    Update wallet.js

--- client/src/pages/wallet.js
+++ client/src/pages/wallet.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+const Wallet = () => {
+  return (
+    <div>
+      <h1>Wallet Dashboard</h1>
+      {/* Wallet management UI will go here */}
+    </div>
+  );
+};
+
+export default Wallet;
