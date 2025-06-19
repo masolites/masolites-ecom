ccba9a65c466a6b64ad62f2b186737f8f90885bd
masolites <mfmmpcs@gmail.com>
2025-06-19 20:15:04 +0100

    Create ico.js

---
+++ client/src/pages/ico.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ICOCountdown from '../components/ICOCountdown';
+
+const ICO = () => {
+  return (
+    <div>
+      <h1>Private Sale ICO</h1>
+      <ICOCountdown />
+      {/* ICO participation UI will go here */}
+    </div>
+  );
+};
+
+export default ICO;
