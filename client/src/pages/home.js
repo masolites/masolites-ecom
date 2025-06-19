7bd0454321da0c6661a406d32f7bd263828d7597
masolites <mfmmpcs@gmail.com>
2025-06-19 20:12:06 +0100

    Update home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>WeeYan: Socials powered by MAZOL</h1>
+      {/* TikTok-like feed will go here */}
+    </div>
+  );
+};
+
+export default Home;
